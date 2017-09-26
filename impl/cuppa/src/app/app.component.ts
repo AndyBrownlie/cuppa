24ad39f4e99d671d97328b66a5a2a75cdddb395f
_Andy_B <andrewbrownlie@gmail.com>
2017-09-26 22:28:28 +0100

    refactored google login button action into component

--- impl/cuppa/src/app/app.component.ts
+++ impl/cuppa/src/app/app.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, NgZone } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MdIconRegistry } from '@angular/material';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements AfterViewInit {
+
+    public title: string = 'app';
+    private clientId: string = "659602254635-j2lfo490qpi9r94pd1tfjnjas9jfs8t6.apps.googleusercontent.com";
+    public googleLoginButtonId: string  = "google-login-button";
+    private userAuthToken: string = null;
+    private userDisplayName: string = "empty";
+    private loggedIn: boolean = false;
+        
+    constructor(private _zone: NgZone) { 
+        console.log(this);
+    }
+
+    // Angular hook that allows for interaction with elements inserted by the
+    // rendering of a view.
+
+    ngAfterViewInit() {
+
+        // Converts the Google login button stub to an actual button.
+        gapi.signin2.render(this.googleLoginButtonId, {
+            "onSuccess": this.onGoogleLoginSuccess,
+            "scope": "profile email",
+            "theme": "dark",
+            "client_id": this.clientId
+        });
+    }
+    
+    // Triggered after a user successfully logs in using the Google external
+    // login provider.
+    onGoogleLoginSuccess = (loggedInUser) => {
+        this._zone.run(() => {
+            this.userAuthToken = loggedInUser.getAuthResponse().id_token;
+            this.userDisplayName = loggedInUser.getBasicProfile().getName();
+            this.loggedIn = true;
+        });
+    }
+    
 }
